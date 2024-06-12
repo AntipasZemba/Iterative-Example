5614992702123c4a7175ecd586d7180c1931efbf
AntipasZemba <169179206+AntipasZemba@users.noreply.github.com>
2024-06-12 18:17:36 -0400

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,96 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtDays = New System.Windows.Forms.TextBox()
+        Me.lblSalary = New System.Windows.Forms.Label()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'txtDays
+        '
+        Me.txtDays.Location = New System.Drawing.Point(126, 39)
+        Me.txtDays.Name = "txtDays"
+        Me.txtDays.Size = New System.Drawing.Size(88, 20)
+        Me.txtDays.TabIndex = 0
+        '
+        'lblSalary
+        '
+        Me.lblSalary.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblSalary.Location = New System.Drawing.Point(116, 71)
+        Me.lblSalary.Name = "lblSalary"
+        Me.lblSalary.Size = New System.Drawing.Size(108, 21)
+        Me.lblSalary.TabIndex = 1
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Location = New System.Drawing.Point(126, 116)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(88, 25)
+        Me.btnCalculate.TabIndex = 2
+        Me.btnCalculate.Text = "Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(55, 42)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(34, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Days:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(55, 72)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(39, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Salary:"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(329, 175)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.lblSalary)
+        Me.Controls.Add(Me.txtDays)
+        Me.Name = "Form1"
+        Me.Text = "How to get rich"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtDays As TextBox
+    Friend WithEvents lblSalary As Label
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,48 @@
+﻿Public Class Form1
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        Dim intDays As Integer
+        Dim dblTotalPay As Double
+
+        Dim blnValidated As Boolean = True
+
+        Get_Validate_Days(intDays, blnValidated)
+
+        If blnValidated Then
+            dblTotalPay = Determine_TotalPay(intDays)
+            Display_TotalPay(dblTotalPay)
+        End If
+    End Sub
+
+    Private Sub Get_Validate_Days(ByRef intDays As Integer, ByRef blnValidated As Boolean)
+        If Integer.TryParse(txtDays.Text, intDays) Then
+            If intDays > 0 Then
+            Else
+                MessageBox.Show("Days must be greater than 0")
+                blnValidated = False
+                txtDays.Focus()
+            End If
+        Else
+            MessageBox.Show("Days must be numeric")
+            blnValidated = False
+            txtDays.Focus()
+        End If
+    End Sub
+
+    Private Function Determine_TotalPay(ByVal intDays As Integer) As Double
+        Dim intCounter As Integer = 1
+        Dim dblSalary As Double = 0.01
+        Dim dblTotalPay As Double
+
+        For intCounter = 1 To intDays
+            dblTotalPay += dblSalary
+
+            dblSalary = dblSalary * 2
+        Next
+
+        Return dblTotalPay
+    End Function
+
+    Private Sub Display_TotalPay(ByVal dblTotalPay As Double)
+        lblSalary.Text = dblTotalPay.ToString("C")
+    End Sub
+End Class
